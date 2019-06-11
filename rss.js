e8b88b8958fd9e9f1218d168829d3050d871c2db
Brian Hanna <brian.hanna@aarcorp.com>
2019-06-10 21:58:16 -0500

    update main rss parser script

--- rss.js
+++ rss.js
@@ -1,6 +1,9 @@
 // Imports
 require('dotenv').config({ path: './config/.env' })
 const fs = require('fs').promises
+const Episode = require('./models').episode
+const EpisodeHost = require('./models').episodehost
+const pretty = require('pretty')
 const FeedService = require('./feed/feed.service')
 const EpisodeService = require('./episode/episode.service')
 const feedService = new FeedService()
@@ -11,6 +14,25 @@ const episodeService = new EpisodeService()
 feedService.get()
   .then(feed => episodeService.filter(feed.items))
   .then(episodes => episodes.map(e => episodeService.parse(e)))
-  .then(parsed => {
-    parsed.forEach(async p => await fs.writeFile(`./data/${p.number}.html`, p.html))
+  .then(async parsed => {
+    parsed.forEach(async p => {
+      await fs.writeFile(`./data/html/${p.number.toString().padStart(3, '0')}.html`, pretty(p.html))
+      const hosts = await Promise.all(p.hosts.map(async h => await episodeService.parseHost(h).then(parsed => parsed.id)))
+      const episode = await Episode.findOrCreate({ where: {
+        number: p.number,
+        title: p.title,
+        html: p.html,
+        description: p.text,
+        publishDate: p.date,
+        guid: p.guid
+      }}).then(e => e[0])
+      // console.log(episode.id, hosts)
+      await hosts.forEach(async host => {
+        console.log(episode.id, host)
+        await EpisodeHost.findOrCreate({
+          where: { episodeId: episode.id, hostId: host },
+          defaults: { episodeId: episode.id, hostId: host }
+        })
+      })
+    })
   })
