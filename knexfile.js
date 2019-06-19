8011da17f43228e771b1991a676efd7a64b9a80a
Brian Hanna <brian.hanna@aarcorp.com>
2019-06-19 08:36:17 -0500

    update knexfile default client to mysql2

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: './config/.env' })
 
 let min, max, pool; [min, max, pool] = [process.env.DB_POOL_MIN || 2, process.env.DB_POOL_MAX || 10, { min, max }]
 const migrations = { tableName: process.env.DB_MIGRATIONS_TABLE_NAME || 'knex_migrations' }
-const client = process.env.DB_CLIENT || 'mysql'
+const client = process.env.DB_CLIENT || 'mysql2'
 const connection = process.env.CLEARDB_DATABASE_URL || {
   socketPath: process.env.DB_SOCKET_PATH,
   user: process.env.DB_USER,
