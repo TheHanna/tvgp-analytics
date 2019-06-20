b81c1f81069ae83b7f0a76ed141cc9ac782e6375
Brian Hanna <brian.hanna@aarcorp.com>
2019-06-20 15:04:27 -0500

    implement easy routes

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require('express')
-const api = require('./api')
+const routes = require('@paciolan/express-easy-routes')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(cors())
-app.get('/', (req, res) => res.sendStatus(404))
-app.get('/episodes', async (req, res) => {
-  const episodes = await api.getEpisodes()
-  res.status(200).send(episodes)
-})
-app.get('/hosts', async (req, res) => {
-  const hosts = await api.getHosts()
-  res.status(200).send(hosts)
-})
+
+routes({ app, path: __dirname + '/**/*.controller.js'})
 
 app.listen(port, err => err ? console.log(err) : console.log(`server is listening on port ${port}`))
